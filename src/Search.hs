1f4b16bb724836831ccdde2f6ab584818d1e8ae5
Turtel216 <papadimitrios216@gmail.com>
2025-05-07 13:48:25 +0300

    Update searchByName to be case insensitive

--- src/Search.hs
+++ src/Search.hs
@@ -2,13 +2,17 @@
 module Search where
 
 import Process
+import Data.Char(toLower)
+import Data.List (isInfixOf)
 
--- Pure function to find dates for a specific name
+-- Function to find nameday dates for a specific (case-insensitive)name
 searchByName :: String -> Root -> [String]
 searchByName searchName root =
   let entries = data_ root
-      matchingEntries = filter (hasName searchName) entries
+      searchNameLower = map toLower searchName
+      matchingEntries = filter (hasName searchNameLower) entries
   in map date matchingEntries
   where
     hasName :: String -> Entry -> Bool
-    hasName nameToFind entry = nameToFind `elem` (name entry)
+    hasName nameToFind entry =
+      any (\n -> nameToFind `isInfixOf` map toLower n) (name entry)
